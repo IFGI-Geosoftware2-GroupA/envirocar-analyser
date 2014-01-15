65172b46cfe88c73a12474db45221950233322bd
dsawatzky <daniel.sawatzky@uni-muenster.de>
2014-01-15 15:54:28 +0100

    Shadow bug solved and some style adjustments

--- js/interactivity.js
+++ js/interactivity.js
@@ -20,6 +20,7 @@ function changeMode() {
 			document.getElementById('logo').src = "img/enviroCarLogo_transparent.png";
 			resizeMap();
 			document.getElementById('search-input').style.display = "block";
+			document.getElementById('logo-label').style.color = "#000";
 			
 			
 		} else {
@@ -33,6 +34,7 @@ function changeMode() {
 			document.getElementById('logo').src = "img/enviroCarLogo_trans_white.png";
 			resizeMap();
 			document.getElementById('search-input').style.display = "block";
+			document.getElementById('logo-label').style.color = "#fff";
 			
 			
 			
@@ -40,3 +42,47 @@ function changeMode() {
 		}
 
 }
+
+/**
+ * jQuery-Function to select a time range 
+ * additonal: datepicker in german language
+ */
+
+$(function() {
+	$( "#calendar-from" ).datepicker({
+		defaultDate: "+1w",
+		changeMonth: true,
+		numberOfMonths: 3,
+		onClose: function( selectedDate ) {
+			$( "#calendar-to" ).datepicker( "option", "minDate", selectedDate );
+		}
+	});
+	$( "#calendar-to" ).datepicker({
+		defaultDate: "+1w",
+		changeMonth: true,
+		numberOfMonths: 3,
+		onClose: function( selectedDate ) {
+			$( "#calendar-from" ).datepicker( "option", "maxDate", selectedDate );
+		}
+	});
+	
+	$.datepicker.regional['de'] = {clearText: 'löschen', clearStatus: 'aktuelles Datum löschen',
+                closeText: 'schließen', closeStatus: 'ohne Änderungen schließen',
+                prevText: '<zurück', prevStatus: 'letzten Monat zeigen',
+                nextText: 'Vor>', nextStatus: 'nächsten Monat zeigen',
+                currentText: 'heute', currentStatus: '',
+                monthNames: ['Januar','Februar','März','April','Mai','Juni',
+                'Juli','August','September','Oktober','November','Dezember'],
+                monthNamesShort: ['Jan','Feb','Mär','Apr','Mai','Jun',
+                'Jul','Aug','Sep','Okt','Nov','Dez'],
+                monthStatus: 'anderen Monat anzeigen', yearStatus: 'anderes Jahr anzeigen',
+                weekHeader: 'Wo', weekStatus: 'Woche des Monats',
+                dayNames: ['Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag'],
+                dayNamesShort: ['So','Mo','Di','Mi','Do','Fr','Sa'],
+                dayNamesMin: ['So','Mo','Di','Mi','Do','Fr','Sa'],
+                dayStatus: 'Setze DD als ersten Wochentag', dateStatus: 'Wähle D, M d',
+                dateFormat: 'dd.mm.yy', firstDay: 1, 
+                initStatus: 'Wähle ein Datum', isRTL: false};
+        $.datepicker.setDefaults($.datepicker.regional['de']);
+});
+
