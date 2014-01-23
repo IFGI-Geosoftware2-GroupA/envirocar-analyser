28266c289061212aa1c898309568245df3804108
Jens13579 <jensbalmert@googlemail.com>
2014-01-23 14:08:38 +0100

    translation in php

---
+++ php/translation.php
@@ -0,0 +1,61 @@
+<?php
+    $_de = "Exploration und Visualisierung";
+	$_en = "Exploration and Visualisation";
+	
+	$_de = "Von";
+	$_en = "From";
+	
+	$_de = "Bis";
+	$_en = "To";
+	
+	$_de = "Zeitraum auswählen";
+	$_en = "Select time period";
+	
+	$_de = "Tracks anzeigen";
+	$_en = "Display tracks";
+	
+	$_de = "Track-ID:";
+	$_en = "Track-ID:";
+	
+	$_de = "Phänomen:";
+	$_en = "Phenomenon:";
+	
+	$_de = "Straßenauswahl aus";
+	$_en = "Street selection on";
+	
+	$_de = "Straßenauswahln an";
+	$_en = "Street selection off";
+	
+	$analyse_modus_label_de = "Analyse Modus";
+	$analyse_modus_label_en = "Analysis Mode";
+	
+    $_de = "Hilfe";
+	$_en = "Help";
+	
+	$_de = "ID";
+	$_en = "ID";
+	
+	$_de = "Verbrauch";
+	$_en = "Consumption";
+	
+	$_de = "CO2-Emission";
+	$_en = "CO2-emissions";
+	
+	$_de = "Geschwindigkeit";
+	$_en = "Speed";
+	
+	$_de = "Luftmasse";
+	$_en = "Air mass";
+	
+	$_de = "Motorlast";
+	$_en = "Engine load";
+	
+	$_de = "GPS-Genauigkeit";
+	$_en = "GPS-Accuracy";
+	
+	$_de = "";
+	$_en = "";
+	
+	$_de = "";
+	$_en = "";
+?>
