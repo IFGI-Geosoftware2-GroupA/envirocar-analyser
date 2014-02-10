fd389fd683eb99ace81e103aa20be56e5441fd44
Jens13579 <jensbalmert@googlemail.com>
2014-02-10 14:57:25 +0100

    Überarbeitung der Variablennamen für die Übersetzung

--- php/translation.php
+++ php/translation.php
@@ -1,62 +1,62 @@
 <?php
-    $explo_und_Visu_de = "Exploration und Visualisierung";
+    $explo_and_Visu_de = "Exploration und Visualisierung";
 	$explo_and_Visu_en = "Exploration and Visualisation";
 	
-	$von_de = "Von";
+	$from_de = "Von";
 	$from_en = "From";
 	
-	$bis_de = "Bis";
+	$to_de = "Bis";
 	$to_en = "To";
 	
-	$zeitraum_de = "Zeitraum auswählen";
+	$select_de = "Zeitraum auswählen";
 	$select_en = "Select Time Period";
 	
-	$tracks_de = "Tracks anzeigen";
+	$display_de = "Tracks anzeigen";
 	$display_en = "Display Tracks";
 	
 	$track_de = "Track-ID:";
 	$track_en = "Track-ID:";
 	
-	$phänomen_de = "Phänomen:";
+	$phenomenon_de = "Phänomen:";
 	$phenomenon_en = "Phenomenon:";
 	
-	$straßen_aus_de = "Straßenauswahl aus";
-	$street_on_en = "Street Selection On";
+	$streets_on_de = "Straßenauswahln An";
+	$streets_on_en = "Street Selection On";
 	
-	$straßen_an_de = "Straßenauswahln an";
+	$streets_off_de = "Straßenauswahl Aus";
 	$streets_off_en = "Street Selection Off";
 	
-	$analyse_modus_label_de = "Analyse Modus";
-	$analyse_modus_label_en = "Analysis Mode";
+	$analysis_mode_label_de = "Analyse Modus";
+	$analysis_mode_label_en = "Analysis Mode";
 	
-    $hilfe_de = "Hilfe";
+    $help_de = "Hilfe";
 	$help_en = "Help";
 	
 	$id_de = "ID";
 	$id_en = "ID";
 	
-	$verbrauch_de = "Verbrauch";
+	$consumption_de = "Verbrauch";
 	$consumption_en = "Consumption";
 	
 	$co2_emission_de = "CO2-Emission";
 	$co2_emission_en = "CO2-Emission";
 	
-	$geschwindigkeit_de = "Geschwindigkeit";
+	$speed_de = "Geschwindigkeit";
 	$speed_en = "Speed";
 	
-	$luftmasse_de = "Luftmasse";
+	$air_mass_de = "Luftmasse";
 	$air_mass_en = "Air Mass";
 	
-	$motorlast_de = "Motorlast";
+	$engine_load_de = "Motorlast";
 	$engine_load_en = "Engine Load";
 	
-	$gps_genauigkeit_de = "GPS-Genauigkeit";
-	$_en = "GPS Accuracy";
+	$gps_accuracy_de = "GPS-Genauigkeit";
+	$gps_accuracy_en = "GPS Accuracy";
 	
-	$gps_höhe_de = "GPS-Höhe";
+	$gps_altitude_de = "GPS-Höhe";
 	$gps_altitude_en = "GPS Altitude";
 	
-	$gps_richtung_de = "GPS-Richtung";
+	$gps_bearing_de = "GPS-Richtung";
 	$gps_bearing_en = "GPS Bearing";
 	
 	$gps_hdop_de = "GPS-HDOP";
@@ -65,52 +65,52 @@
 	$gps_pdop_de = "GPS-PDOP";
 	$gps_pdop_en = "GPS PDOP";
 	
-	$gps_geschwindigkeit_de = "GPS-Geschwindigkeit";
+	$gps_speed_de = "GPS-Geschwindigkeit";
 	$gps_speed_en = "GPS Speed";
 	
 	$gps_vdop_de = "GPS VDOP";
 	$gps_vdop_en = "GPS-VDOP";
 	
-	$einlassdruck_de = "Einlassdruck";
+	$intake_pressure_de = "Einlassdruck";
 	$intake_pressure_en = "Intake Pressure";
 	
-	$einlasstemperatur_de = "Einlasstemperatur";
+	$intake_temperature_de = "Einlasstemperatur";
 	$intake_temperature_en = "Intake Temperature";
 	
-	$lmm_de = "LMM";
+	$maf_de = "LMM";
 	$maf_en = "MAF";
 	
-	$upm_de = "UpM";
+	$rpm_de = "UpM";
 	$rpm_en = "Rpm";
 	
-	$geschwindigkeit_de = "Geschwindigkeit";
+	$speed_de = "Geschwindigkeit";
 	$speed_en = "Speed";
 	
-	$dksp_de = "Drosselklappenstellungsposition";
+	$throttle_position_de = "Drosselklappenstellungsposition";
 	$throttle_position_en = "Throttle Position";
 	
-	$ksak_de = "Kraftstoffanlagenkreislauf";
+	$fsl_de = "Kraftstoffanlagenkreislauf";
 	$fsl_en = "Fuel System Loop";
 	
-	$ksasc_de = "Kraftstoffanlagenstatus-Code";
+	$fssc_de = "Kraftstoffanlagenstatus-Code";
 	$fssc_en = "Fuel System Status Code";
 	
-	$lzkt_de = "Langzeitkraftstofftrimm";
+	$ltft_de = "Langzeitkraftstofftrimm";
 	$ltft_en = "Long-Term Fuel Trim";
 	
-	$o2_lambda_strömung_de = "O2-Lambda-Strömung";
+	$o2_lambda_current_de = "O2-Lambda-Strömung";
 	$o2_lambda_current_en = "O2 Lambda Current";
 	
-	$o2_lambda_ströung_er_de = "O2-Lambda-Ströung ER";
+	$o2_lambda_current_er_de = "O2-Lambda-Ströung ER";
 	$o2_lambda_current_er_en = "O2 Lambda Current ER";
 	
-	$o2_lambda_spannung_de = "O2-Lambda-Spannung";
-	$_o2_lambda_voltage_en = "O2 Lambda Voltage";
+	$o2_lambda_voltage_de = "O2-Lambda-Spannung";
+	$o2_lambda_voltage_en = "O2 Lambda Voltage";
 	
-	$o2_lambda_spannung_er_de = "O2-Lambda-Spannung-ER";
+	$o2_lambda_voltage_er_de = "O2-Lambda-Spannung-ER";
 	$o2_lambda_voltage_er_en = "O2 Lambda Voltage ER";
 	
-	$kkt_de = "Kurzzeitkraftstofftrimm";
+	$stft_1_de = "Kurzzeitkraftstofftrimm";
 	$stft_1_en = "Short-Term Fuel Trim 1";
 	
 ?>
