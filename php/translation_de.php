1a5364493a53435920d874264c592364aff46acb
Jens13579 <jensbalmert@googlemail.com>
2014-02-11 15:28:48 +0100

    Übersetzung in zwei Dokumente gesplittet

---
+++ php/translation_de.php
@@ -0,0 +1,79 @@
+<?php
+    
+    $explo_and_Visu_de = "Exploration und Visualisierung";
+	
+	$from_de = "Von";
+	
+	$to_de = "Bis";
+	
+	$select_de = "Zeitraum auswählen";
+	
+	$display_de = "Tracks anzeigen";
+	
+	$track_de = "Track-ID:";
+	
+	$phenomenon_de = "Phänomen:";
+	
+	$streets_on_de = "Straßenauswahln An";
+	
+	$streets_off_de = "Straßenauswahl Aus";
+	
+	$analysis_mode_label_de = "Analyse Modus";
+	
+    $help_de = "Hilfe";
+	
+	$id_de = "ID";
+	
+	$consumption_de = "Verbrauch";
+	
+	$co2_emission_de = "CO2-Emission";
+	
+	$speed_de = "Geschwindigkeit";
+	
+	$air_mass_de = "Luftmasse";
+	
+	$engine_load_de = "Motorlast";
+	
+	$gps_accuracy_de = "GPS-Genauigkeit";
+	
+	$gps_altitude_de = "GPS-Höhe";
+
+	$gps_bearing_de = "GPS-Richtung";
+	
+	$gps_hdop_de = "GPS-HDOP";
+	
+	$gps_pdop_de = "GPS-PDOP";
+
+	$gps_speed_de = "GPS-Geschwindigkeit";
+	
+	$gps_vdop_de = "GPS VDOP";
+	
+	$intake_pressure_de = "Einlassdruck";
+	
+	$intake_temperature_de = "Einlasstemperatur";
+
+	$maf_de = "LMM";
+
+	$rpm_de = "UpM";
+	
+	$speed_de = "Geschwindigkeit";
+	
+	$throttle_position_de = "Throttle-Position";
+
+	$fsl_de = "Kraftstoffanlagenkreislauf";
+
+	$fssc_de = "Kraftstoffanlagenstatus-Code";
+	
+	$ltft_de = "Langzeitkraftstofftrimm";
+	
+	$o2_lambda_current_de = "O2-Lambda-Strömung";
+	
+	$o2_lambda_current_er_de = "O2-Lambda-Ströung ER";
+	
+	$o2_lambda_voltage_de = "O2-Lambda-Spannung";
+
+	$o2_lambda_voltage_er_de = "O2-Lambda-Spannung-ER";
+	
+	$stft_1_de = "Kurzzeitkraftstofftrimm 1";
+	
+?>
