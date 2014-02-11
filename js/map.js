584cb30683925bfa8b2dd476012cd0baa04a54d8
Marius Runde <mrunde@users.noreply.github.com>
2014-02-11 16:39:20 +0100

    Alerts arent prompted when map is changed to OSM

--- js/map.js
+++ js/map.js
@@ -114,11 +114,13 @@ function initMap() {
 	 * Listen for changed map type id to enable/disable collecting street segments
 	 * Only Google Maps are allowed to be used for the Google Directions API
 	 */
+	/*
 	google.maps.event.addListener(map, 'maptypeid_changed', function() {
 		if (map.getMapTypeId() == 'OSM') {
 			alert('Only Google Maps are allowed to be used for the Google Directions API');
 		}
 	});
+	*/
 	
 	// Listen for the search box results
 	google.maps.event.addListener(searchBox, 'places_changed', function() {
