4aa55b78fe3729ae69c32a2f0fb0d7f19d4c7d95
Marius <marius.runde@uni-muenster.de>
2014-02-18 20:43:52 +0100

    First try to connect table rows with markers Also formatted the table.js according to code standards

--- js/map.js
+++ js/map.js
@@ -185,17 +185,16 @@ function showMarkers(query) {
 					position : measurements[i].getPoint(),
 					icon : 'img/circle.png'
 				});
+				marker.id = measurements[i].getId();
 				markers.push(marker);
 				markersBounds.extend(measurements[i].getPoint());
 				// Create infowindow for marker[i]/measurement[i]
 				buildInfoWindow(marker,map,measurements[i]);
-				google.maps.event.addListener(marker, 'click', function(){
-					var mid = measurements[i].getId();
-					var bg = document.createAttribute("backgroundColor");
-					bg.nodeValue = '#B0C4DE';
-					document.getElementById(mid).setAttributeNode(bg);
-					
-				});
+				/*google.maps.event.addListener(marker, 'click', function(){
+					// var bg = document.createAttribute("backgroundColor");
+					// bg.nodeValue = '#B0C4DE';
+					document.getElementById(marker.id).style.backgroundColor = '#B0C4DE';
+				});*/
 			}
 
 			var mcOptions = {
