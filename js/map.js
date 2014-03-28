3df341607709e2281c22e1f1ac4df6a7affe039b
MarioGerdes <m_gerd06@uni-muenster.de>
2014-03-28 16:34:32 +0100

    Little Changes on max size BB

--- js/map.js
+++ js/map.js
@@ -612,18 +612,12 @@ function initBoundingBox() {
 			bounds : bounds,
 			editable : true,
 			draggable : true,
-			strokeColor: '#FF0000',
-   			strokeOpacity: 0.8,
-    		strokeWeight: 2,
-    		fillColor: '#FF0000',
-    		fillOpacity: 0.35,
 		});
 		
 
 		rectangle.setMap(map);
 		
-		//Hide the GetData Button, because the first automatic initialized BoundingBox is too big
-		document.getElementById('selectTimeBtn').style.visibility = 'hidden';
+		showSize();
 		
 		//Eventlistener for changing the size of the BoundingBox
 		google.maps.event.addListener(rectangle, 'bounds_changed', showSize);
